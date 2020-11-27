a8d5fdf4465334f10612acb4d9cb10c749f4cbba
Al Mustarik <almustarik@gmail.com>
2020-11-27 11:51:43 +0600

    Polls Modification

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(@5xdpb-7s9v2!jnz6o%1ox$sacg+k-c1qutjesm__qc(b6x*s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
